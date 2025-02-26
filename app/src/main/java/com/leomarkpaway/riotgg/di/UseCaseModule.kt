f9082ae08dcbeff351ac9c98c4ada098e2ba9594
leomarkpaway <leomarkpaway@gmail.com>
2025-02-27 03:31:35 +0800

    🐛 Fix use case module issue

--- app/src/main/java/com/leomarkpaway/riotgg/di/UseCaseModule.kt
+++ app/src/main/java/com/leomarkpaway/riotgg/di/UseCaseModule.kt
@@ -5,11 +5,12 @@ import com.leomarkpaway.riotgg.domain.usecase.FetchChampionDetailsUseCase
 import com.leomarkpaway.riotgg.domain.usecase.FetchChampionListUsaCase
 import com.leomarkpaway.riotgg.presentation.champion_details.ChampionDetailsViewModel
 import com.leomarkpaway.riotgg.presentation.champion_list.ChampionListViewModel
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val useCaseModule = module {
-    single<FetchChampionListUsaCase> { FetchChampionListUsaCase(repository = get()) }
-    single<ChampionListViewModel> { ChampionListViewModel(fetchChampionListUseCase = get()) }
-    single<FetchChampionDetailsUseCase> { FetchChampionDetailsUseCase(repository = get()) }
-    single<ChampionDetailsViewModel> { ChampionDetailsViewModel(fetchChampionDetailsUseCase = get()) }
+    factory<FetchChampionListUsaCase> { FetchChampionListUsaCase(repository = get()) }
+    viewModel<ChampionListViewModel> { ChampionListViewModel(fetchChampionListUseCase = get()) }
+    factory<FetchChampionDetailsUseCase> { FetchChampionDetailsUseCase(repository = get()) }
+    viewModel<ChampionDetailsViewModel> { ChampionDetailsViewModel(fetchChampionDetailsUseCase = get()) }
 }
